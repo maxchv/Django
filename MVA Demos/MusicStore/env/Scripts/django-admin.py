05861af07ac05304f5a5c1179721e66895ba96f5
GeekTrainer <chrhar@microsoft.com>
2015-06-16 09:05:41 -0700

    Initial update

---
+++ MVA Demos/MusicStore/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\geekt\Source\Repos\Django\MVA Demos\MusicStore\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
